e34191374d439e0679bb7cc7394783229bbd966f
choiyewon <choiyewon1997@gmail.com>
2023-10-21 10:04:41 +0900

    #1. mysql 연동 설정

---
+++ src/main/java/com/example/mini_pro/MiniProApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProApplication.class, args);
+    }
+
+}
